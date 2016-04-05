78b82df704edb7233321aac7e3125412ee3103e3
Salakomg <salakorte@outlook.es>
2016-04-05 18:14:38 +0200

    mi super codigo de medidas :)

---
+++ Ejercicios/src/ejercicios/Medidas2.java
@@ -0,0 +1,60 @@
+package ejercicios;
+
+import javax.swing.JOptionPane;
+
+public class Medidas2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String pregunta = JOptionPane.showInputDialog("¿Dime una cantidad en kilometros y lo pasare s todas las unidades");
+		
+		int a = Integer.parseInt(pregunta);
+		
+	    int b = a * 10;
+	     
+	    String opcion = JOptionPane.showInputDialog("Ahora te lo pasare en la unidad que tu me digas(hm, dam, m, dm, cm, mm)");
+	    
+	    if(opcion.equals("hm")){
+	    
+	    	JOptionPane.showMessageDialog(null, "En hm es: " + b);
+	    }
+	  
+	    if(opcion.equals("dam")){
+	    	
+	    	double h = a*100;
+	    
+	    	JOptionPane.showMessageDialog(null, "En dam es: " + h);
+	    }
+	    
+	    if(opcion.equals("m")){
+	       
+	    	double j = a * 1000;
+	    
+	        JOptionPane.showMessageDialog(null, "En m es: " + j);
+	    }
+	    
+	    if(opcion.equals("dm")){
+	    	double k = a * 10000;
+	        JOptionPane.showMessageDialog(null,"En dm es: " + k);
+	    }
+	    
+	    if(opcion.equals("cm")){
+	    	
+	    	double q = a * 100000;
+	    	
+	    	JOptionPane.showMessageDialog(null, "En cm es: " + q);
+	    	
+	    }
+
+	    if(opcion.equals("mm")){
+	    	double  w = a * 1000000;
+	        JOptionPane.showMessageDialog(null, "En mm es: " + w);
+	    }
+	    
+	}
+}	    
+	    
+	
+	
+      
