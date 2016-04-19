c9c1fd8ffaa65a3cc2c601a8877422a308003252
Salakomg <salakorte@outlook.es>
2016-04-19 18:32:46 +0200

    la conversacion  es de preguntas

--- conversacion/src/conversacion/conversacion.java
+++ conversacion/src/conversacion/conversacion.java
@@ -6,41 +6,58 @@ public class Conversacion {
 
 	public static void main(String[] args) {
 
-
-
+		//
+		//
+		
 		String q = JOptionPane.showInputDialog("풲e gusta minecraft?");
 
 		if (q.equals("si")){
-			
+
 			String w = JOptionPane.showInputDialog( "Y el grand thef auto");
 
 			if (w.equals("si")){
 				String r = JOptionPane.showInputDialog("Y el Scrap mechanic");
 				if (r.equals("si")){
-					JOptionPane.showMessageDialog(null, "Entonces eres gamer");
+					JOptionPane.showMessageDialog(null, "Entonces eres gamer");	
+					if (r.equals("no")) {
+						String y = JOptionPane.showInputDialog("풲e gusta jugar a video juegos?");
+						if (y.equals("")) {
+
+						}
+					}
+
 				}
 			}
-			
 		}
-		
-		
-		if (q.equals("no")) {
 
+
+
+
+		if (q.equals("no")) {
+			String w = JOptionPane.showInputDialog( "Y el grand thef auto");
 			String e = JOptionPane.showInputDialog( "퓍e gusta jugar a video juegos?");
-			if (e.equals("si")) 
-				JOptionPane.showMessageDialog(null,"A entonces vale :)"); 
-				if (e.equals("no")) {
-					JOptionPane.showMessageDialog(null, "Pues me caes mal >:(");
-					
+			if (e.equals("si")){ 
+				JOptionPane.showMessageDialog(null,"A entonces vale :)");
+			}
+			if (e.equals("no")) {
+				JOptionPane.showMessageDialog(null, "Pues me caes mal >:(");
+
+				if (w.equals("no")) {
+					String t = JOptionPane.showInputDialog("풲e gusta jugar a video juegos?");
+					if (t.equals("si")) {
+						JOptionPane.showConfirmDialog(null, "A entonces vale :)");	
+						if (t.equals("no")) {
+							JOptionPane.showMessageDialog(null, "Pues me caes mal >:)");
+
+
+						}
+
+
+					}
 				}
 			}
-
-		}	 
-		
-		
-		
+		}
 	}
-
-
+}
 
 
